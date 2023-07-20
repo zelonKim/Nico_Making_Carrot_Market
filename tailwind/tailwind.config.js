386b3fa61066c229211daec891f43317b341bcf0
zelonKim <ksz18601@gmail.com>
2023-07-20 14:09:57 +0900

    tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [ // Tailwind를 사용할 파일들을 명시해줌.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
