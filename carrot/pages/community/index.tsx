e8f01c97ad4d33664f92feec8891172f8b3dcdb5
zelonKim <ksz18601@gmail.com>
2023-08-02 14:22:39 +0900

    making community

--- carrot/pages/community/index.tsx
+++ carrot/pages/community/index.tsx
@@ -7,29 +7,34 @@ const Community: NextPage = () => {
   return (
     <Layout hasTabBar title="동네생활">
       <div className="space-y-4 divide-y-[2px]">
-        {[1, 2, 3, 4, 5, 6].map((_, i) => (
-          <Link key={i} href={`/community/${i}`}>
-            <a className="flex cursor-pointer flex-col pt-4 items-start">
+        
+        {[1, 2, 3, 4, 5].map((_, i) => (
+          <Link
+            key={i}
+            href={`/community/${i}`}
+            className="flex flex-col items-start pt-4 cursor-pointer"
+          >
               <span className="flex ml-4 items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
                 동네질문
               </span>
-              <div className="mt-2 px-4 text-gray-700">
-                <span className="text-orange-500 font-medium">Q.</span> What is
-                the best mandu restaurant?
+            
+              <div className="px-4 mt-2 text-gray-700">
+                <span className="font-medium text-orange-500">Q.</span> What is the best mandu restaurant?
               </div>
-              <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
+            
+              <div className="flex items-center justify-between w-full px-4 mt-5 text-xs font-medium text-gray-500">
                 <span>니꼬</span>
                 <span>18시간 전</span>
               </div>
+              
               <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t   w-full">
-                <span className="flex space-x-2 items-center text-sm">
+                <span className="flex items-center space-x-2 text-sm">
                   <svg
                     className="w-4 h-4"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
+                    xmlns="http://www.w3.org/2000/svg">
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -37,16 +42,16 @@ const Community: NextPage = () => {
                       d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                     ></path>
                   </svg>
-                  <span>궁금해요 1</span>
+                  <span> 궁금해요 1 </span>
                 </span>
-                <span className="flex space-x-2 items-center text-sm">
+              
+                <span className="flex items-center space-x-2 text-sm">
                   <svg
                     className="w-4 h-4"
                     fill="none"
                     stroke="currentColor"
                     viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
+                    xmlns="http://www.w3.org/2000/svg">
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -56,10 +61,11 @@ const Community: NextPage = () => {
                   </svg>
                   <span>답변 1</span>
                 </span>
-              </div>
-            </a>
+              
+            </div>
           </Link>
         ))}
+
         <FloatingButton href="/community/write">
           <svg
             className="w-6 h-6"
@@ -76,6 +82,7 @@ const Community: NextPage = () => {
             ></path>
           </svg>
         </FloatingButton>
+        
       </div>
     </Layout>
   );
